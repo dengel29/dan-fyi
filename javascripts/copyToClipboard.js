0c4cbef125ce5ed8bf9237ec9688d521b705c7e9
Dan Engel <dengel29@gmail.com>
2018-11-30 16:21:35 +0800

    Automated commit at 2018-11-30 08:21:35 UTC by middleman-deploy 2.0.0-alpha

--- javascripts/copyToClipboard.js
+++ javascripts/copyToClipboard.js
@@ -1,6 +1,11 @@
+ // TODO
+
+ // Listen for click, add element after clickable content to add the "copied successfully" message
+
  var clipboard = new ClipboardJS('.btn');
     clipboard.on('success', function(e) {
         console.log(e);
+        document.elemeng
     });
     clipboard.on('error', function(e) {
         console.log(e);

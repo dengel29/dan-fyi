b1b360d69721e48f4c0d88ac59205296545c64fa
Dan Engel <dengel29@gmail.com>
2019-02-01 15:07:45 +0800

    added qr code tippy to project

--- source/javascripts/projects.js
+++ source/javascripts/projects.js
@@ -1,30 +1,17 @@
 // projects.js
 
+let detectUserAgent = function() {
+  let ua = window.navigator.userAgent
+  console.log(ua)
+  if (ua.match(/Android/i)|| ua.match(/webOS/i) || ua.match(/iPhone/i) || ua.match(/iPad/i) || ua.match(/iPod/i) || ua.match(/BlackBerry/i) || ua.match(/Windows Phone/i)) {
+    return "click"
+  }
+  else {
+    return "mouseenter focus"
+  }
+}
 
- // var project = document.getElementById('project-sticker')
- // project.addEventListener('click', function(e){
- //  console.log(e.target)
- // })
-
-
-// var expandContent = function(e) {
-//   var project = e.target.parentNode.id
-//   var projEl = document.getElementById(project)
-//   console.log(e)
-
-//   projEl.children[1].classList.toggle('uncollapse')
-// }
-
-// const buttons = document.getElementsByClassName('card-head')
-
-// for (let button of buttons) {
-//   button.addEventListener('click', expandContent)
-// }
-
-
-//////////
-
-var expandContent = function(e) {
+let expandContent = function(e) {
   // console.log(e.target.parentNode.id)
   console.log(e.target)
   if ((e.target.parentNode.id) || (e.target.parentNode.parentNode.parentNode.id)) {
@@ -40,6 +27,27 @@ var expandContent = function(e) {
 const projectsContainer = document.querySelector('.projects')
 projectsContainer.addEventListener('click', expandContent)
 
+// create the containing div for the tooltip
+const container = document.createElement('div')
+
+// create the simple label text
+const label = document.createTextNode("Scan with WeChat to try!")
+console.log(label)
+// create the QR code image element
+let stickerQR = new Image(150,150);
+stickerQR.src = '../images/qr-wx-stickermachine.jpg';
+stickerQR.setAttribute("style", "border-radius:5px")
+
+container.appendChild(label)
+container.insertBefore(stickerQR, label)
 
 
-
+tippy('.tippy-qr', {
+  content: container,
+  hideOnClick: true,
+  interactive: true,
+  maxWidth:150,
+  animation: "shift-toward",
+  trigger: detectUserAgent(),
+  placement: "right"
+})
